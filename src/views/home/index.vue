7293d0e7116d6a624238ecf6833584241e122f7b
mjf <JianFeng.Mao@aliyun.com>
2023-12-11 22:52:34 +0800

    添加eslint

---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div style="display: flex; color: aliceblue" class="a" />
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+div {
+  color: red;
+
+  &.active {
+    color: black;
+  }
+}
+</style>
