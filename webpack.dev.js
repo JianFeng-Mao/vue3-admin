4c8e2659f6cce8cbe659cc7687aa952a6b6e5b54
mjf <JianFeng.Mao@aliyun.com>
2023-12-16 23:40:40 +0800

    refacto: 修改项目结构及webpack.dev配置

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: mjf JianFeng.Mao@aliyun.com
+ * @Date: 2023-12-10 21:38:02
+ * @LastEditors: mjf JianFeng.Mao@aliyun.com
+ * @LastEditTime: 2023-12-16 23:36:39
+ * @FilePath: \vue3-admin\webpack.dev.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { merge } = require('webpack-merge');
 const path = require('path');
@@ -25,7 +33,8 @@ module.exports = merge(commonConfig, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html')
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html'
     })
   ],
   stats: 'minimal'
