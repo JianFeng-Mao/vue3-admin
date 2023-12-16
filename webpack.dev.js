0188dec8bb99b733ced112bd77cf9b4ef8283f78
mjf <JianFeng.Mao@aliyun.com>
2023-12-16 23:45:20 +0800

    chore: 移除自动注释

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,3 @@
-/*
- * @Author: mjf JianFeng.Mao@aliyun.com
- * @Date: 2023-12-10 21:38:02
- * @LastEditors: mjf JianFeng.Mao@aliyun.com
- * @LastEditTime: 2023-12-16 23:36:39
- * @FilePath: \vue3-admin\webpack.dev.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { merge } = require('webpack-merge');
 const path = require('path');
